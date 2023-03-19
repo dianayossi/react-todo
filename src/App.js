aa8b1c024b8d0b7ab29633298ea4d0736f3ae52f
dianayossi <dycarachure@gmail.com>
2023-03-18 20:53:36 -0700

    Lesson 1-1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+const todoList = [
+  {
+    id: 1,
+    title: "Begin reading assignment",
+  },
+  {
+    id: 2,
+    title: "Begin coding assignment",
+  },
+  {
+    id: 3,
+    title: "Turn in assignment",
+  }
+];
+
+function App(){
+  return(
+    <div>
+      <h1>Todo List</h1>
+      <hr></hr>
+      <ul>
+        {todoList.map(function(item){
+          return(
+            <li key={item.id}>
+                {/* <span>{item.id}</span> */}
+                <span>{item.title}</span>
+            </li>
+          )
+        })}
+
+        
+      </ul>
     </div>
   );
 }
